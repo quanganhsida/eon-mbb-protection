5ea9ec1fafebffb1df00bf28af06f64e6427efc3
Tran Quang Anh <tranquanganh.cv@gmail.com>
2026-07-17 19:32:09 +0200

    x

---
+++ data/raw/orange/Outputs/script.sh
@@ -0,0 +1,12 @@
+sbatch jobsbatch_6.sh
+echo jobsbatch_6.sh >> okBatchs.txt
+sbatch jobsbatch_5.sh
+echo jobsbatch_5.sh >> okBatchs.txt
+sbatch jobsbatch_2.sh
+echo jobsbatch_2.sh >> okBatchs.txt
+sbatch jobsbatch_4.sh
+echo jobsbatch_4.sh >> okBatchs.txt
+sbatch jobsbatch_3.sh
+echo jobsbatch_3.sh >> okBatchs.txt
+sbatch jobsbatch_1.sh
+echo jobsbatch_1.sh >> okBatchs.txt
