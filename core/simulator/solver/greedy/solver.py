32c883f0828e9ea1c2fd8766af4e68c11dddea24
Quang Anh Tran <tranquanganh.cv@gmail.com>
2026-07-20 05:29:33 +0200

    x

--- core/simulator/solver/greedy/solver.py
+++ core/simulator/solver/greedy/solver.py
@@ -30,6 +30,21 @@ def get_failure_edges(env):
         for u, v in getattr(env, "failure_links", [])
     }
 
+def build_initial_occupation(env):
+#     build the current spectrum occupation from nominal solution
+
+    occupation = defaultdict(dict)
+
+    for demand_id_str, nominal in env.nominal_paths.items():
+        path       = nominal["path"]
+        slot_block = nominal["slot_block"]
+
+        for edge in path_to_edges(path):
+            for slot in slot_block:
+                occupation[edge][slot] = int(demand_id_str)
+
+    return occupation
+
 
 def solve_greedy_resilient_rsa(env, solution_path: str):
 #     1. Take the affected demands K_Z.
@@ -43,6 +58,16 @@ def solve_greedy_resilient_rsa(env, solution_path: str):
 
     start_time = perf_counter()
 
-    # get affected demands from failure zone
+#     get affected demands from failure zone
     affected_demands = list(getattr(env, "affected_demands", []))
     failure_edges    = get_failure_edges(env)
+
+#      gen migration order randomly
+    migration_order = affected_demands[:]
+    random.Random(RANDOM_SEED).shuffle(migration_order)
+
+#     build the current occupation from nominal solution
+    occupation = build_initial_occupation(env)
+
+    migrated_paths = {}
+    failed_demands = []
