2c7b1494230c520d7fcd6ff1495541135b660e63
Tran Quang Anh <tranquanganh.cv@gmail.com>
2026-08-19 11:16:13 +0200

    x

--- core/util.py
+++ core/util.py
@@ -7,7 +7,7 @@ def get_args():
     parser.add_argument("--instance_file", type=str, default="../data/instance/scenario3.json", help="Path to the instance JSON file.",)
     parser.add_argument("--figure_file", type=str, default="../data/figure/scenario3_topology.pdf", help="Path to save the output figure.",)
     parser.add_argument("--figure_dir", type=str, default="../data/figure/orange", help="Directory to save output figures.",)
-    parser.add_argument("--plot_type", type=str, default="topology", choices=["topology", "solution", "solve", "result", "result_by_demand"], help="Type of plot to generate.",)
+    parser.add_argument("--plot_type", type=str, default="topology", choices=["topology", "solution", "solve", "solve_greedy", "result", "result_by_demand"], help="Type of plot to generate.",)
     parser.add_argument("--solution_file", type=str, default="../data/solution/protection/scenario3_basic_solution.json", help="Path to save the optimization solution.",)
 
     return parser.parse_args()
